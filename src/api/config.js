c290c990b2a7cc46ff26497f54bf2c902190aa93
Trần Minh Hiếu <hieuhienlanh03@gmail.com>
2023-02-21 01:14:10 +0700

    update test ECMA

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instace = axios.create({
-    baseURL: "https://j0osum-8080.preview.csb.app/api",
+    baseURL: "https://dj0r17-8080.preview.csb.app/api",
 });
 
 export default instace
