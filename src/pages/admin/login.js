31126a8880c6d5335ed55f84355cd17840ba26a1
Trần Minh Hiếu <hieuhienlanh03@gmail.com>
2023-02-20 16:16:31 +0700

    update test ECMA

--- src/pages/admin/login.js
+++ src/pages/admin/login.js
@@ -38,7 +38,7 @@ const login = () => {
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Document</title>
-      <link rel="stylesheet" href="../brandy/css/login.css">
+      <link rel="stylesheet" href="./brandy/css/login.css">
   </head>
   <body>
       <div class="container">
