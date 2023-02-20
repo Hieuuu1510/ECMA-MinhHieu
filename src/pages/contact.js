64deee385ef936aba1634036884610e90e9b1fb9
Trần Minh Hiếu <hieuhienlanh03@gmail.com>
2023-02-20 15:36:19 +0700

    update test ECMA

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -28,7 +28,7 @@ const ContactPage = () => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-        <link rel="stylesheet" href="././brandy/css/contact.css">
+        <link rel="stylesheet" href="./brandy/css/contact.css">
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
